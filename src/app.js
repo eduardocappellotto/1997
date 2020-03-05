80cff3e38b0ea1feada649536c2aa250f714b787
eduardocappellotto <51893328+eduardocappellotto@users.noreply.github.com>
2020-03-05 15:00:36 -0300

    Adicionado modelo Order

--- src/app.js
+++ src/app.js
@@ -9,19 +9,19 @@ const router = express.Router();
 
 // Conecta ao MongoDB Atlas
 
-mongoose.connect('mongodb+srv://edu123:edu123@cluster0-mipxi.mongodb.net/test?retryWrites=true&w=majority',
-{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false } )
+mongoose.connect('mongodb+srv://edu123:edu123@cluster0-mipxi.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
 
 //Carrega os models
 
 const Product = require('./models/product');
+const Customer = require('./models/customer');
 
 // Carrega as Rotas
 const indexRoute = require('./routes/index-route');
 const productRoute = require('./routes/product-route');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 
 app.use('/', indexRoute);
