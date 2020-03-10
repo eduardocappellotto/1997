197ae44ac4f99a99c91aed35b65e1f7df660eba1
eduardocappellotto <51893328+eduardocappellotto@users.noreply.github.com>
2020-03-10 17:20:52 -0300

    Update order-repository.js
    exemplo de populate nos itens da order

--- src/repositories/order-repository.js
+++ src/repositories/order-repository.js
@@ -4,7 +4,10 @@ const mongoose = require('mongoose')
 const Order = mongoose.model('Order');
 
 exports.get = async(data) => {
-    var res = await Order.find({});
+    var res = await Order
+        .find({}, 'number status customer items')
+        .populate('customer', 'name')
+        .populate('items.product', 'title');
     return res;
 }
 
