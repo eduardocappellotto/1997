d0f09f61119b393e7fd31248e617c38f7216e466
eduardocappellotto <51893328+eduardocappellotto@users.noreply.github.com>
2020-03-10 17:25:02 -0300

    Update customer-controller.js
    controlador de autenticação de cliente

--- src/controllers/customer-controller.js
+++ src/controllers/customer-controller.js
@@ -39,4 +39,37 @@ exports.post = async(req, res, next) => {
         res.status(500).send({ message: "Falha ao processar sua requisição" },
             console.log(e));
     }
+exports.authenticate = async(req, res, next) => {
+
+    try {
+        const customer = await repository.authenticate({
+            email: req.body.email,
+            password: md5(req.body.password + global.SALT_KEY)
+        });
+
+        if (!customer) {
+            res.status(404).send({
+                message: 'Usuário ou senha inválidos'
+            });
+            return
+        }
+        const token = await authService.generateToken({
+            id: customer._id,
+            email: customer.email,
+            name: customer.name,
+            roles: customer.roles
+        })
+
+        res.status(201).send({
+            token: token,
+            data: {
+                email: customer.email,
+                name: customer.name
+            }
+        });
+    } catch (e) {
+        res.status(500).send({ message: "Falha ao processar sua requisição" },
+            console.log(e));
+    }
+};
 };
